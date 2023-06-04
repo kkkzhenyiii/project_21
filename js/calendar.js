d3e7a09db708a4e871139058fe1012897b7f6062
kkkzhenyiii <kangzhenyi77@gmail.com>
2023-06-04 19:21:19 +0800

    visit--calendar

---
+++ js/calendar.js
@@ -0,0 +1,64 @@
+const currentDate = document.querySelector(".current_date"),
+daysTag = document.querySelector(".days"),
+prevNextIcon = document.querySelectorAll(".icons span");
+
+// get new date, current year and month
+let date = new Date(),
+currYear = date.getFullYear(),
+currMonth = date.getMonth();
+
+// console.log(date, currYear, currMonth);
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"];
+
+
+const renderCalender = () => {
+    // dynamic days
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+    // console.log(lastDateofMonth);
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+
+    let liTag = "";
+
+    for (let i = firstDayofMonth; i > 0; i--){ // creating li of previous month last days
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+    }
+
+
+
+    for (let i = 1; i <= lastDateofMonth; i++){ // creating li of all days of current month
+        // console.log(i); // check i 
+        // adding active class to li, if the current day, month, and year matched
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i}</li>`;
+    }
+
+    for (let i = lastDayofMonth; i < 6; i++){ // creating li of next month first days
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+    }
+
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
+}
+renderCalender();
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener("click", () => { // adding click event on both icons
+        // console.log(icon); // check click event
+
+        // if clicked icon is "prev" then decrement current month by 1, else increment it by 1
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+        if (currMonth < 0 || currMonth > 11){ // if current month is less than 0 or greater than 11
+            // creating a new date of current year & month and pass it as date value
+            date = new Date(currMonth, currYear);
+            currYear = date.getFullYear(); // updating current year with new date year
+            currMonth = date.getMonth(); // updating current month with new date month
+        }else { // else pass new Date as date value
+            date = new Date();
+        }
+        renderCalender();
+    })
+});
